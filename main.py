ece745444c3425c336a1962214b591b08987d97c
tri-jet <sahatrijit123@gmail.com>
2024-10-21 20:26:57 +0530

    Open birthday and reveal secret at right date

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bot import input_birthday
 error_count = 0
 
-secret_bday = "13/6/2000"
+f = open('birthday.txt','r')
+secret_bday = f.readline()
 
 while(error_count < 3):
     user_input = input("Please input your birthday to reveal your secret letter.")
@@ -9,21 +10,23 @@ while(error_count < 3):
     
     # if correct format:
     if bot_result[0] == 'Y':
-        bot_result = bot_result[2:]
+        bot_result = bot_result[3:]
         if bot_result == secret_bday:
             print("reveal secret")
+            break
         else: 
             print("incorrect birthday, no secret for you")
             
     # incorrect format
     elif bot_result[0] == 'N':
-        print(bot_result[2:]) 
+        print(bot_result[3:]) 
     else:
         print("unknown bot output" + bot_result)
     
     error_count += 1
-    
-print("3 incorrect attempts -> secret will no longer be revealed.")
+
+if error_count > 2:
+    print("3 incorrect attempts -> secret will no longer be revealed.")
 
 ## Pass user input to bot + determine how bot output received.
 
