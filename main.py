aceda52251142d7ec9dd4548bf1d0d5d295402bb
tri-jet <sahatrijit123@gmail.com>
2024-10-21 19:59:01 +0530

    Add conditions to determine whether input correct etc

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from bot import input_birthday
 error_count = 0
 
+secret_bday = "13/6/2000"
+
 while(error_count < 3):
     user_input = input("Please input your birthday to reveal your secret letter.")
-    input_birthday(user_input=user_input)
-    # send data to bot
-    # if correct format:
-        # check if matches birthday.txt
+    bot_result = input_birthday(user_input=user_input)
     
-    # else: error_count += 1, 
-    print("done")
-    error_count = 3
+    # if correct format:
+    if bot_result[0] == 'Y':
+        bot_result = bot_result[2:]
+        if bot_result == secret_bday:
+            print("reveal secret")
+        else: 
+            print("incorrect birthday, no secret for you")
+            
+    # incorrect format
+    elif bot_result[0] == 'N':
+        print(bot_result[2:]) 
+    else:
+        print("unknown bot output" + bot_result)
+    
+    error_count += 1
+    
+print("3 incorrect attempts -> secret will no longer be revealed.")
 
 ## Pass user input to bot + determine how bot output received.
 
