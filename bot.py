4679de9398e62f87eac588af6e4342b6068f6ac6
tri-jet <sahatrijit123@gmail.com>
2024-10-22 16:52:00 +0530

    Change how bot responds to valid mm/dd/yy input

--- bot.py
+++ bot.py
@@ -22,8 +22,10 @@ def input_birthday(user_input):
             2. if the date is missing the day, month, or year, tell them what's missing, responding with N: <your feedback> in the language they chose \
             3. if the date uses invalid dates e.g. non-existent months like 13, february with more than 28/29 days or a year in the future, respond \
                with N: <feedback on what makes the date invalid> in the language they chose \
-            4. if the date can be interpreted as valid in multiple date formats e.g. dd/mm/yy vs mm/dd/yy, respond with A: <clarification question, \
-               asking which date they meant using the full word form for clarity in the language they chose \
+            4. if the date uses invalid dates e.g. non-existent months like 13, but the date would be valid in different formats i.e. mm/dd/yy  \
+               e.g. 6/13/2000 - would be valid as 13th june 2000, so respond with A: <clarification asking if they meant the valid date form of their input>\
+            5. if the date in different formats e.g. dd/mm/yy and mm/dd/yy e.g. 5/6/2000 could be 5th June or 6th May,  respond with A: <clarification question, \
+               asking which date they meant using the full word form for clarity in the language they chose\
              "},
             {"role": "user", "content": user_input}
         ], 
